949dea326fd5fb971ded7b8a3fe495cce6d29517
Chris Pavlina <cpavlin1@binghamton.edu>
2014-11-03 21:30:40 -0500

    Replaced Excel chart with PyLab.

---
+++ testing/adc/adc_inl.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# Rough test of INL in ATSAM4S16C ADC
+# Observation: ADC output spans full range from 0 to 4095.
+
+from pylab import *
+
+test_voltages = arange (0.2, 3.3, 0.2)
+adc_out = [
+    244, 490, 736, 982, 1228, 1474, 1720, 1966, 2212,
+    2458, 2704, 2950, 3196, 3442, 3689, 3935, ];
+
+def tee (text, f):
+    f.write (text)
+    print (text, end='')
+
+
+# Compute linear fit
+fit_slope, fit_inter = polyfit (test_voltages, adc_out, 1)
+
+# Compute ideal outputs under perfect linearity
+adc_out_ideal = fit_slope * test_voltages + fit_inter
+
+# Residuals
+resid = adc_out_ideal - adc_out
+
+plot (test_voltages, adc_out)
+title ('ATSAM4S16C ADC Output vs Input')
+xlabel ('Input (V)')
+ylabel ('Output (LSB)')
+grid (True)
+savefig ('adc_out.eps')
+savefig ('adc_out.png', dpi=72)
+close ()
+
+plot (test_voltages, resid)
+title ('ATSAM4S16C ADC Nonlinear Residuals')
+xlabel ('Input (V)')
+ylabel ('Residual (LSB)')
+grid (True)
+savefig ('adc_resid.eps')
+savefig ('adc_out.png', dpi=72)
+close ()
+
+max_inl = max (abs (resid))
+
+with open ('transcript.txt', 'w') as f:
+    tee ("Maximum INL (LSB): %f\n" % max_inl, f)
