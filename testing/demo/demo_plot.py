fb3522c14f3f71fe4e7050d32cc682da0bcabbfb
Chris Pavlina <cpavlin1@binghamton.edu>
2014-12-11 09:44:07 -0500

    More demo rework

--- testing/demo/demo_plot.py
+++ testing/demo/demo_plot.py
@@ -44,8 +44,11 @@ s.write (b"T:CH 1\r\n")
 time.sleep (0.5)
 print ("  OK")
 
+lower_bound = float (sys.argv[1])
+upper_bound = float (sys.argv[2])
+
 print ("Collecting data... ")
-freqs = np.logspace(3, 8.1761, 60) # 1 kHz to 150 MHz
+freqs = np.logspace(np.log10(lower_bound), np.log10(upper_bound), 60) # 1 kHz to 150 MHz
 data = []
 for i in freqs:
     s.write (("T:FREQ 1, %f\r\n" % i).encode ('ascii'))
@@ -60,93 +63,98 @@ data = [i-data[0] for i in data]
 
 plt.subplot(2, 1, 1)
 #ax = plt.axes(xlim=(1e3, 1e9))
-plot, = plt.semilogx (freqs, data)
+if 'linear' in sys.argv:
+    plot, = plt.plot (freqs, data)
+else:
+    plot, = plt.semilogx (freqs, data)
 plt.xlabel ("Frequency (Hz)")
 plt.ylabel ("Amplitude (dB, uncalibrated)")
 plt.title ("Voltage Insertion Gain, uncalibrated")
 plt.grid (True)
 
-#plt.draw ()
-#plt.waitforbuttonpress (-1)
-#
-print ("Collecting phase data...")
-N_POINTS_PER_RANGE = 4
-PRECISION = 10.
-freqs = np.logspace(3, 8.1761, 20) # 1 kHz to 150 MHz
-data = []
-for i in freqs:
-    # Set frequency. Then, search phases for a null
-    s.write (("T:FREQ 1, %f\r\n" % i).encode ('ascii'))
-    getline (s)
-    s.write (("T:FREQ 0, %f\r\n" % i).encode ('ascii'))
-    getline (s)
-    s.write (b"T:AMP 1, 0.5\r\n")
-    getline (s)
-    s.write (b"T:AMP 0, 0.1\r\n")
-    getline (s)
+if "phase" in sys.argv:
+    plt.draw ()
+    plt.waitforbuttonpress (-1)
 
-    phase_bound_left = 0
-    phase_bound_right = 360
-    while phase_bound_right - phase_bound_left > PRECISION:
-        width = phase_bound_right - phase_bound_left
-        pitch = width / N_POINTS_PER_RANGE
-        phases = [(i * pitch) + phase_bound_left for i in range (N_POINTS_PER_RANGE)]
-        lowest_phase = None
-        lowest_amp = float("inf")
-        lowest_i = None
-        for phase_i, phase in enumerate(phases):
-            phase = phase % 360.
-            s.write (("T:PHASE 0, %f\r\n" % phase).encode ('ascii'))
-            getline (s)
-            s.write (b"T:SAM 5000\r\n")
-            level = float (getline (s))
-            if level < lowest_amp:
-                lowest_amp = level
-                lowest_phase = phase
-                lowest_i = phase_i
-            print (".", end='')
-            sys.stdout.flush ()
-        if lowest_i == 0:
-            # Slide the range left
+    print ("Collecting phase data...")
+    N_POINTS_PER_RANGE = 4
+    PRECISION = 10.
+    freqs = np.logspace(np.log10(lower_bound), np.log10(upper_bound), 10)
+    data = []
+    for i in freqs:
+        # Set frequency. Then, search phases for a null
+        s.write (("T:FREQ 1, %f\r\n" % i).encode ('ascii'))
+        getline (s)
+        s.write (("T:FREQ 0, %f\r\n" % i).encode ('ascii'))
+        getline (s)
+        s.write (b"T:AMP 1, 0.5\r\n")
+        getline (s)
+        s.write (b"T:AMP 0, 0.1\r\n")
+        getline (s)
+
+        phase_bound_left = 0
+        phase_bound_right = 360
+        while phase_bound_right - phase_bound_left > PRECISION:
             width = phase_bound_right - phase_bound_left
-            phase_bound_left -= width / 2
-            phase_bound_right -= width / 2
-        elif lowest_i == len(phases) - 1:
-            # Slide the range right
-            width = phase_bound_right - phase_bound_left
-            phase_bound_left += width / 2
-            phase_bound_right += width / 2
-        else:
-            # Narrow the range
-            phase_bound_left = phases[lowest_i - 1]
-            phase_bound_right = phases[lowest_i + 1]
+            pitch = width / N_POINTS_PER_RANGE
+            phases = [(i * pitch) + phase_bound_left for i in range (N_POINTS_PER_RANGE)]
+            lowest_phase = None
+            lowest_amp = float("inf")
+            lowest_i = None
+            for phase_i, phase in enumerate(phases):
+                phase = phase % 360.
+                s.write (("T:PHASE 0, %f\r\n" % phase).encode ('ascii'))
+                getline (s)
+                s.write (b"T:SAM 5000\r\n")
+                level = float (getline (s))
+                if level < lowest_amp:
+                    lowest_amp = level
+                    lowest_phase = phase
+                    lowest_i = phase_i
+                print (".", end='')
+                sys.stdout.flush ()
+            if lowest_i == 0:
+                # Slide the range left
+                width = phase_bound_right - phase_bound_left
+                phase_bound_left -= width / 2
+                phase_bound_right -= width / 2
+            elif lowest_i == len(phases) - 1:
+                # Slide the range right
+                width = phase_bound_right - phase_bound_left
+                phase_bound_left += width / 2
+                phase_bound_right += width / 2
+            else:
+                # Narrow the range
+                phase_bound_left = phases[lowest_i - 1]
+                phase_bound_right = phases[lowest_i + 1]
 
-    phases = list (range (0, 360, 40))
-    lowest_phase -= 180.
+        phases = list (range (0, 360, 40))
+        lowest_phase -= 180.
+        phase = lowest_phase
 
-    # Smooth phase discontinuities
-    if len(data):
-        last_phase = data[-1]
-        offset_phases = [phase + (360. * i) for i in range(-3,4)]
-        phase_errors = [abs(i - last_phase) for i in offset_phases]
+        # Smooth phase discontinuities
+        if len(data):
+            last_phase = data[-1]
+            offset_phases = [phase + (360. * i) for i in range(-3,4)]
+            phase_errors = [abs(i - last_phase) for i in offset_phases]
 
-        best_phase, best_error = min (zip (offset_phases, phase_errors), key=lambda x: x[1])
-        phase = best_phase
+            best_phase, best_error = min (zip (offset_phases, phase_errors), key=lambda x: x[1])
+            phase = best_phase
 
 
-    print ("%.2f Hz\t%f deg" % (i, phase))
-    data.append (phase)
+        print ("%.2f Hz\t%f deg" % (i, phase))
+        data.append (phase)
 
-plt.subplot(2, 1, 2)
-#ax = plt.axes(xlim=(1e3, 1e9))
-plot, = plt.semilogx (freqs, data)
-plt.xlabel ("Frequency (Hz)")
-plt.ylabel ("Phase (deg, uncalibrated)")
-plt.title ("Phase Shift, uncalibrated")
-plt.grid (True)
+    plt.subplot(2, 1, 2)
+    #ax = plt.axes(xlim=(1e3, 1e9))
+    plot, = plt.semilogx (freqs, data)
+    plt.xlabel ("Frequency (Hz)")
+    plt.ylabel ("Phase (deg, uncalibrated)")
+    plt.title ("Phase Shift, uncalibrated")
+    plt.grid (True)
 
-plt.savefig('out.png')
-plt.show ()
+    plt.savefig('out.png')
+    plt.show ()
 #plt.waitforbuttonpress (-1)
 
 
