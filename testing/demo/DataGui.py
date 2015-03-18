ac72ff145bf9bdd33f1e2a76063bca6e017e8fd7
Harrison <howens1@binghamton.edu>
2015-03-18 10:17:43 -0400

    changed to correct version of DataGui

--- testing/demo/DataGui.py
+++ testing/demo/DataGui.py
@@ -1,39 +1,43 @@
 #!usr/bin/python
-
-from Tkinter import *
-import tkMessageBox
+try:
+	from Tkinter import *
+except ImportError: 
+	from tkinter import *
+ 
+##from tkinter import messagebox
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-##style.use("ggplot")
-##f = Figure(figsize=(5,5), dpi=100)
-##a = f.add_subplot(111)
-
+ 
 top =  Tk()
 frame =  Frame(top)
-frame.pack()
-
-phases = [12,32,42,23, 42,12,34]
+frame.pack() 
+## ####test values THIS MUST BE REPLACED#######
+phases = ['12','32','42','23', '42','12','34']
+phases2 = [12,32,42,23,42,12,34]
 freq = [2,3,4,5,6,7,8]
+###############################################
 
+##phase = Label(top, text=phases)
+##phase.pack()
+
+## Buttons that still need to be wrapped around a SPI input#######
 bottomframe = Frame(top)
 bottomframe.pack( side = BOTTOM)
-
-redbutton = Button(frame, text="INCR", fg="Black")
-redbutton.pack( side = LEFT )
-
-brownbutton = Button(frame, text="FREQ", fg="Black")
-brownbutton.pack( side = LEFT )
-
-bluebutton = Button(frame, text="PHASE", fg="Black")
-bluebutton.pack( side = LEFT )
-
-blackbutton = Button(frame, text="GRAPH", fg="Black")
-blackbutton.pack( side = LEFT )
-
-text = Text(top)
-text.insert([phases],'dfdfdf')
-text.pack(side = BOTTOM)
-
-plot = plt.plot (phases,frequency)
  
+#####################################################
+
+## outputting arbitrary values read in list in text widget will need to wrap  on Chris function 
+
+def textInsert():
+	text = Text(top)
+	for x in phases:
+		text.insert(END, x + '\n')
+##text.insert([phases],'dfdfdf')
+		text.pack(side = BOTTOM)
+###############################################################################################
+	plt.plot (phases)
+	plt.axis([0,8,0,40])
+	plt.show()
+startbutton = Button(frame, text= "Start", fg= "Black", command = textInsert)
+startbutton.pack(side = TOP)
 top.mainloop()
